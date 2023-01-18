169ab017c08ef1746c45d3b1e38e05323c4bafc8
Alexey Koleda <alexeykoleda@icloud.com>
2023-01-18 13:41:54 +0300

    Initial Commit

---
+++ DemoPurchase/DemoPurchaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoPurchaseApp.swift
+//  DemoPurchase
+//
+//  Created by Alexey Koleda on 18.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct DemoPurchaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
