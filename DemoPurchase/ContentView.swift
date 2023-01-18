4af976635487f99e5bc9444d9499cd90402d5f44
Alexey Koleda <alexeykoleda@icloud.com>
2023-01-18 14:09:59 +0300

    Add view with purchases

--- DemoPurchase/ContentView.swift
+++ DemoPurchase/ContentView.swift
@@ -6,16 +6,40 @@
 //
 
 import SwiftUI
+import StoreKit
 
 struct ContentView: View {
+    private let productIds = ["pro_monthly", "pro_yearly", "pro_lifetime"]
+
+    @State
+    private var products: [Product] = []
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            Text("Products")
+            ForEach(self.products) { product in
+                Button {
+                    // Don't do anything yet
+                } label: {
+                    Text("\(product.displayPrice) - \(product.displayName)")
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(.blue)
+                        .clipShape(Capsule())
+                }
+            }
+        }.task {
+            do {
+                try await self.loadProducts()
+            } catch {
+                print(error)
+            }
         }
-        .padding()
+    }
+
+    private func loadProducts() async throws {
+        // Products are not returned in the order the ids are requested
+        self.products = try await Product.products(for: productIds)
     }
 }
 
