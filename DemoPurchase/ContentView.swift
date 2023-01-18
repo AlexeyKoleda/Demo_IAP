2833792abace2db086352e677268220612f92018
Alexey Koleda <alexeykoleda@icloud.com>
2023-01-18 14:44:23 +0300

    Add 'purchase' function and add it into the purchase buttons action

--- DemoPurchase/ContentView.swift
+++ DemoPurchase/ContentView.swift
@@ -19,7 +19,13 @@ struct ContentView: View {
             Text("Products")
             ForEach(self.products) { product in
                 Button {
-                    // Don't do anything yet
+                    Task {
+                         do {
+                             try await self.purchase(product)
+                         } catch {
+                             print(error)
+                         }
+                     }
                 } label: {
                     Text("\(product.displayPrice) - \(product.displayName)")
                         .foregroundColor(.white)
@@ -40,6 +46,29 @@ struct ContentView: View {
     private func loadProducts() async throws {
         // Products are not returned in the order the ids are requested
         self.products = try await Product.products(for: productIds)
+    }
+    
+    private func purchase(_ product: Product) async throws {
+        let result = try await product.purchase()
+
+        switch result {
+        case let .success(.verified(transaction)):
+            // Successful purhcase
+            await transaction.finish()
+        case let .success(.unverified(_, error)):
+            // Successful purchase but transaction/receipt can't be verified
+            // Could be a jailbroken phone
+            break
+        case .pending:
+            // Transaction waiting on SCA (Strong Customer Authentication) or
+            // approval from Ask to Buy
+            break
+        case .userCancelled:
+            // ^^^
+            break
+        @unknown default:
+            break
+        }
     }
 }
 
